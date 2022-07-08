03821292719ffc41d1f97fcf51bad0eb6533e38f
yamashita-kllc <yamashita@kllc.jp>
2022-07-08 17:29:00 +0900

    Add: test script and style

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <h1>Hello, World!!</h1>
+  <p>{{ num }}</p>
+  <button @click="++num">+</button>
+  <button @click="--num">-</button>
 </template>
+
+<script setup lang="ts">
+const num = ref(1)
+</script>
+
+<style>
+button {
+  width: 30px;
+  margin: 4px;
+}
+</style>
