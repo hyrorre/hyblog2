864c93db22a6b65b4f93d96184f1d5741a249cdd
yamashita-kllc <yamashita@kllc.jp>
2022-07-08 18:03:11 +0900

    Add: bootstrap

--- app.vue
+++ app.vue
@@ -1,17 +1,14 @@
 <template>
   <h1>Hello, World!!</h1>
   <p>{{ num }}</p>
-  <button @click="++num">+</button>
-  <button @click="--num">-</button>
+  <button @click="++num" class="btn btn-primary m-2" style="width: 60px">+</button>
+  <button @click="--num" class="btn btn-primary m-2" style="width: 60px">-</button>
 </template>
 
 <script setup lang="ts">
 const num = ref(1)
 </script>
 
-<style>
-button {
-  width: 30px;
-  margin: 4px;
-}
+<style lang="scss">
+@import 'node_modules/bootstrap/scss/bootstrap';
 </style>
