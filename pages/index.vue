f5564b2de1d21c9c9567f490b75490ecaee1189a
yamashita-kllc <yamashita@kllc.jp>
2022-07-13 00:44:29 +0900

    Add: post excerpt in index page

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,9 @@
     <ContentList v-slot="{ list }">
       <article v-for="post in list" :key="post._path" class="py-4 border-bottom">
         <nuxt-link :to="post._path" class="link-dark text-decoration-none">
-          <h2>{{ post.title }}</h2>
+          <h2 class="pb-4">{{ post.title }}</h2>
         </nuxt-link>
-        <div>{{ post.description }}</div>
-        <img :src="post.cover" />
+        <ContentDoc :path="post._path" excerpt class="content-doc"></ContentDoc>
       </article>
     </ContentList>
     <ul class="pagination justify-content-center mt-4">
