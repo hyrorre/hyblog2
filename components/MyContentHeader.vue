1061fb03574c9b090d753b524e87025f77c07c67
yamashita-kllc <yamashita@kllc.jp>
2022-11-19 05:48:32 +0900

    Add: profile page

--- components/MyContentHeader.vue
+++ components/MyContentHeader.vue
@@ -5,13 +5,13 @@
       {{ new Date(page.date).toLocaleDateString('sv-SE') }}
     </div>
     <div class="d-flex flex-wrap">
-      <div class="me-2">
+      <div v-if="page.categories" class="me-2">
         <i class="bi bi-folder me-2"></i>
         <nuxt-link v-for="category in page.categories" :key="category" :to="`/category/${category}`" class="me-2">
           {{ category }}
         </nuxt-link>
       </div>
-      <div class="me-2">
+      <div v-if="page.tags" class="me-2">
         <i class="bi bi-tags me-2"></i>
         <nuxt-link v-for="tag in page.tags" :key="tag" :to="`/tag/${tag}`" class="me-2">
           {{ tag }}
---
+++ content/profile.md
@@ -0,0 +1,24 @@
+---
+title: Profile
+date: 2022-11-19
+---
+
+hyrorre – すたろあ  
+インターネット音ゲーマー兼Webエンジニア
+
+<img src="https://hyrorre.com/wp-content/uploads/2018/01/profile-300x300.jpg" style="width: initial" alt="Profile Image">
+
+## Develop
+- 現在はフリーランスWebエンジニアとして活動中
+- 学生時代はC++でゲーム作ったり、C#/VueJSで攻略ツール作ったりしてました
+- [製作物一覧](/products)
+
+## Game
+- 音楽ゲーム
+  - beatmania IIDX
+  - その他BEMANI系もちょっとだけ
+- FPS / TPS
+  - Splatoon
+  - Apex Legends
+- ソーシャルゲーム
+  - ブルーアーカイブ -Blue Archive-
