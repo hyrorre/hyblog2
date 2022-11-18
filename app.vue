c9ffc59240ca0fb8dece4aeb3d0030b0373e945f
yamashita-kllc <yamashita@kllc.jp>
2022-11-19 06:37:55 +0900

    Update: contact form with Microsoft Forms

--- app.vue
+++ app.vue
@@ -1,29 +1,39 @@
 <template>
-  <header class="navbar navbar-dark bg-dark">
-    <div class="container">
-      <nuxt-link class="navbar-brand" to="/">HY Techs</nuxt-link>
-      <ul class="navbar-nav flex-row gap-4">
-        <li class="nav-item">
-          <nuxt-link class="nav-link" to="/profile">Profile</nuxt-link>
-        </li>
-        <li class="nav-item">
-          <nuxt-link class="nav-link" to="/products">Products</nuxt-link>
-        </li>
-        <li class="nav-item">
-          <nuxt-link class="nav-link" to="/contact">Contact</nuxt-link>
-        </li>
-      </ul>
-    </div>
-  </header>
-  <main class="container">
-    <NuxtPage></NuxtPage>
-  </main>
-  <footer class="text-center bg-light py-4">© 2020-{{ new Date().getFullYear() }} hyrorre</footer>
+  <div class="d-flex flex-column h-100">
+    <header class="navbar navbar-dark bg-dark">
+      <div class="container">
+        <nuxt-link class="navbar-brand" to="/">HY Techs</nuxt-link>
+        <ul class="navbar-nav flex-row gap-4">
+          <li class="nav-item">
+            <nuxt-link class="nav-link" to="/profile">Profile</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link class="nav-link" to="/products">Products</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link class="nav-link" to="/contact">Contact</nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </header>
+    <main class="container flex-grow-1">
+      <NuxtPage></NuxtPage>
+    </main>
+    <footer class="text-center bg-light py-4">© 2020-{{ new Date().getFullYear() }} hyrorre</footer>
+  </div>
 </template>
 
 <style lang="scss">
 @import 'bootstrap/scss/bootstrap';
 @import 'bootstrap-icons/font/bootstrap-icons';
+
+html,
+body,
+#__nuxt,
+#__layout {
+  height: 100% !important;
+  width: 100% !important;
+}
 
 .link-dark:hover {
   color: $secondary !important;
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,27 +1,8 @@
 <template>
-  <div class="py-4">
-    <h1>Contact</h1>
-    <p class="mt-3">お問い合わせ等があれば、こちらのフォームへ必要事項を記入し、送信してください。</p>
-    <form @submit="submit">
-      <div class="mb-3">
-        <label for="name" class="form-label">名前またはニックネーム</label>
-        <input class="form-control" id="name" />
-      </div>
-      <div class="mb-3">
-        <label for="email" class="form-label">メールアドレス</label>
-        <input type="email" class="form-control" id="email" />
-      </div>
-      <div class="mb-3">
-        <label for="subject" class="form-label">件名</label>
-        <input class="form-control" id="subject" />
-      </div>
-      <div class="mb-3">
-        <label for="body" class="form-label">メッセージ本文</label>
-        <textarea class="form-control" id="body" style="height: 120px"></textarea>
-      </div>
-      <button class="btn btn-primary" type="submit">送信</button>
-    </form>
-  </div>
+  <iframe
+    src="https://forms.office.com/r/NK8vQ7YmfF"
+    style="margin: 0 calc(50% - 50vw); width: 100vw; height: 100%"
+  ></iframe>
 </template>
 
 <script setup lang="ts">
